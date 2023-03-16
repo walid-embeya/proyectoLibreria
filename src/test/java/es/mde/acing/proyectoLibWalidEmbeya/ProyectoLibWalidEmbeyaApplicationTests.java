03b4c7613b039f1a5e81d29548e1581de8061531
walid <embeya2009@yahoo.fr>
2023-03-16 12:59:35 +0100

    segundo revisar build.gradle

--- src/test/java/es/mde/acing/proyectoLibWalidEmbeya/ProyectoLibWalidEmbeyaApplicationTests.java
+++ src/test/java/es/mde/acing/proyectoLibWalidEmbeya/ProyectoLibWalidEmbeyaApplicationTests.java
@@ -1,12 +1,12 @@
 package es.mde.acing.proyectoLibWalidEmbeya;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ProyectoLibWalidEmbeyaApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
