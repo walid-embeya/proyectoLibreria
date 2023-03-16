7a129e839e556a18ff2ed0254bd0400f7b7f969e
walid <embeya2009@yahoo.fr>
2023-03-16 12:10:52 +0100

    primer commit Lib

---
+++ src/main/java/es/mde/acing/proyectoLibWalidEmbeya/ProyectoLibWalidEmbeyaApplication.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibWalidEmbeya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLibWalidEmbeyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLibWalidEmbeyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mde/acing/proyectoLibWalidEmbeya/ProyectoLibWalidEmbeyaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibWalidEmbeya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLibWalidEmbeyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
